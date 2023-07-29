8bde5e20e2ff73e33c6539c7eae78e2f157eb5d8
Yohannan PC <yohannanpchacko0814@gmail.com>
2023-07-29 20:08:58 +0530

    2nd time

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 ## create a simple flask application
 from flask import Flask, render_template, url_for,request, redirect
+#This is push no1
 ## This is added in vs code
 ## create the flask app
 app=Flask(__name__)
